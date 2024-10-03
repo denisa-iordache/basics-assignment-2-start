3d0bb5a7f02e5f6f47451f2d5b06933b7d839178
Denisa <denisa.iordache@getik.net>
2024-10-03 13:38:55 +0300

    hasfjahs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  username = 'Enter ther username';
+  username = 'Enter a username';
 
   resetUsername(){
     this.username = '';
